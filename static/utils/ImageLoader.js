8f7f052dc329f7c85ca5f2e7436e9af72d3c8580
gleneder <geder@purdue.edu>
2020-04-13 17:02:16 -0400

    Adds getSprite function

--- static/utils/ImageLoader.js
+++ static/utils/ImageLoader.js
@@ -17,3 +17,10 @@ function doneLoading() {
     console.log("ImageLoader: finished loading sprites.")
 
 }
+
+//returns PIXI sprite given the provied file link
+function getSprite(imgFile) {
+
+    return new PIXI.Sprite(PIXI.loader.resources[imgFile].texture)
+
+}
