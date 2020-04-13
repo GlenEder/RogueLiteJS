e6b2e7d3bd307921c64e710b96287d7e086515ef
gleneder <geder@purdue.edu>
2020-04-13 19:54:47 -0400

    Correctly adds double array for walkable

--- static/objects/Room.js
+++ static/objects/Room.js
@@ -7,15 +7,39 @@ class Room {
         this.height = height
         this.numWalkable = numWalkable
 
-        this.map = [height][width]
+        //create two dimension map
+        this.walkableMap = []
+        for(var i = 0; i < height; i++) {
+            var inner = []
+            for(var j = 0; j < width; j++) {
+                inner.push(false)
+            }
+            this.walkableMap.push(inner)
+        }
 
         this.generateRoom()
     }
 
 
+    //creates a random layout for the walkable map
     generateRoom() {
         console.log("Room: Generating room")
+
+        //pick random starting tile
+        let startX = Math.floor(Math.random() * this.width)
+        let startY = Math.floor(Math.random() * this.height)
+
+        //set starting tile to walkable
+        this.walkableMap[startY][startX] = true
+
     }
+
+
+    //logs walkable map 
+    printWalkableMap() {
+        
+    }
+
 
 
 }
