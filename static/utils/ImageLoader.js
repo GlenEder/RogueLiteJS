b42a3da6555bbab5cc78d4a7281b9db1d2a4f790
gleneder <geder@purdue.edu>
2020-04-13 18:53:43 -0400

    Maps sprites from sheet

--- static/utils/ImageLoader.js
+++ static/utils/ImageLoader.js
@@ -12,6 +12,29 @@ function loadSprite(key, imgFile) {
 //maps with key: <tileKey>_<tileNumber>
 function loadSheet(pixiRef, sheetFile, tileW, tileH, numTiles, tileKey) {
 
-    
+    //load sheet file into cache 
+    pixiRef.loader.add(tileKey, sheetFile)
+    .load( () => {
+
+        //get ref to sheet in cache
+        let sheet = new PIXI.BaseTexture.from(pixiRef.loader.resources[tileKey].url)
+
+        //create sprites from png file
+        for(var i = 0; i < numTiles; i++) {
+
+            let texture = new PIXI.Texture(sheet, new PIXI.Rectangle(i * tileW, 0, tileW, tileH))
+
+            let key = tileKey + "_" + i
+            console.log(key)
+
+            let sprite = new PIXI.Sprite(texture)
+
+            addSpriteToMap(key, sprite)
+        }
+
+
+        console.log("ImageLoader: Done adding sprites from - " + sheetFile)
+
+    })
 
 }
