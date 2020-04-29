efcbc21fe23d632be608cca2a9ddc8fcae9b02be
gleneder <geder@purdue.edu>
2020-04-29 19:40:48 -0400

    Removes sprite stuff from constructor of tile

--- static/objects/Tile.js
+++ static/objects/Tile.js
@@ -5,13 +5,16 @@ class Tile {
     /*
     x -- x position of tile in walkable grid
     y -- y position of tile in walkable grid
-    spriteName -- name for sprite for getting from image handler
-    spriteDir -- direction sprite faces (1: no rot, 2: 90deg, 3: 180deg, 4: 270deg) 
+    isWalkable -- if tile is walkable by player & npcs
     */
-    constructor(x, y, walkable, spriteName, spriteDir) {
+    constructor(x, y, isWalkable) {
         this.x = x
         this.y = y
         this.walkable = walkable
+        this.sprite = null
+    }
+
+    setSprite(spriteName, spriteDir) {
         this.sprite = getSprite(spriteName)
         this.sprite.rotation = (90 * (spriteDir - 1))
     }
