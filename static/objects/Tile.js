c61f653060499f68ad03f027592d82c8783e6964
gleneder <geder@purdue.edu>
2020-04-29 19:51:13 -0400

    adds render method and updates setSprite

--- static/objects/Tile.js
+++ static/objects/Tile.js
@@ -14,10 +14,17 @@ class Tile {
         this.sprite = null
     }
 
+    //Set sprite for tile and rotation 
     setSprite(spriteName, spriteDir) {
         this.sprite = getSprite(spriteName)
         this.sprite.rotation = (90 * (spriteDir - 1))
+        this.sprite.x = this.x * this.sprite.width
+        this.sprite.y = this.y * this.sprite.height
     }
 
+    //Returns sprite of tile with screen cords
+    render() {
+        return this.sprite
+    }
 
 }
