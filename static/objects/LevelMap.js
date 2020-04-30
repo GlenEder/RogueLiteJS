40c44c663a7cefd9b7eac124b6fe8bffcb16abde
gleneder <geder@purdue.edu>
2020-04-30 18:14:30 -0400

    Adds constructor to levelmap

--- static/objects/LevelMap.js
+++ static/objects/LevelMap.js
@@ -3,5 +3,15 @@
 class LevelMap {
 
 
-    
+    constructor(numRooms) {
+
+        this.numRooms = numRooms        //number of rooms level will have
+        this.walkables = new Map();     //map of tiles that player can walk on 
+
+        //create container
+        this.container = new PIXI.Container()
+
+    }
+
+
 }
